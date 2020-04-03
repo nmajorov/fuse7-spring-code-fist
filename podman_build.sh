02deff8878c27a460f1040f532421f7fee9bdba8
Nikolaj Majorov <nikolaj@majorov.biz>
2020-04-03 13:08:44 +0200

    change podman build

--- podman_build.sh
+++ podman_build.sh
@@ -4,6 +4,11 @@ echo "build using podman "
 
 TAG="6.2.2-jdk8"
 CONTAINER="docker.io/library/gradle:$TAG"
+CACHE_DIR="/tmp/.gradle"
 
-podman  run --rm  -v "$PWD":/home/gradle/project:Z -w /home/gradle/project $CONTAINER  gradle build
+mkdir -p "$CACHE_DIR"
+
+podman  run --rm -it  \
+    -v "$CACHE_DIR":/home/gradle/.gradle:Z  \
+    -v "$PWD":/home/gradle/project:Z -w /home/gradle/project $CONTAINER  $@
 
